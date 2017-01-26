ca4c122ee34f4988b99f4bedbb83ada38c97e0a1
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-01-26 17:30:45 +0400

    Customizer JS file added

--- includes/classes/class-customizer.php
+++ includes/classes/class-customizer.php
@@ -4,7 +4,7 @@
  *
  * @author      Mahdi Yazdani
  * @package     Hypermarket
- * @since       1.0
+ * @since       1.0.1
  */
 if (!defined('ABSPATH')):
     exit;
@@ -19,7 +19,7 @@ if (!class_exists('Hypermarket_Customizer')):
         /**
          * Setup class.
          *
-         * @since 1.0
+         * @since 1.0.1
          */
         public function __construct()
 
@@ -28,10 +28,10 @@ if (!class_exists('Hypermarket_Customizer')):
                 $this,
                 'customize_register'
             ) , 10);
-            add_action('customize_controls_print_footer_scripts', array(
+            add_action('customize_controls_enqueue_scripts', array(
                 $this,
-                'print_footer_scripts'
-            ) , 999);
+                'enqueue'
+            ) , 10);
         }
         /**
          * Theme Customizer along with several other settings.
@@ -83,55 +83,27 @@ if (!class_exists('Hypermarket_Customizer')):
             ));
         }
         /**
-         * Display Hypermarket Plus back-link in the Customizer controls header.
+         * Enqueue scripts and styles.
          *
-         * @since 1.0
+         * @since 1.0.1
          */
-        public function print_footer_scripts()
+        public function enqueue()
 
         {
+            wp_register_script('hypermarket-customizer', get_template_directory_uri() . '/assets/admin/js/hypermarket-customizer.js', array(
+                'jquery'
+            ) , HypermarketThemeVersion, true);
+            wp_localize_script('hypermarket-customizer', 'hypermarket_upsell', array(
+                'upsellURL' => hypermarket_sanitize_url(HypermarketThemeAuthorURI) ,
+                'upsellLabel' => __('Looking for more options?', 'hypermarket')
+            ));
             if (!class_exists('BS3_Grid_Builder_Activation')):
-                $bs3_grid_builder = hypermarket_sanitize_url(get_admin_url()) . 'plugin-install.php?s=bs3+grid+builder&tab=search&type=term';
-?>
-                <script type="application/javascript">
-                    (function($) {
-                        $(document).ready(function() {
-                            var hypermarket_plus = $('<br/><a class="get-hypermarket-plus"></a>')
-                                .attr('href', '<?php echo $bs3_grid_builder; ?>')
-                                .attr('target', '_blank')
-                                .text('<?php esc_html_e('Get Free Grid Builder!', 'hypermarket'); ?>')
-                                .css({
-                                    'display' : 'inline-block',
-                                    'position' : 'relative',
-                                    'padding' : '3px 6px',
-                                    'line-height' : '1.5',
-                                    'font-size' : '9px',
-                                    'letter-spacing' : '1px',
-                                    'text-transform' : 'uppercase',
-                                    '-webkit-font-smoothing' : 'subpixel-antialiased',
-                                    'color' : '#cdbfe3',
-                                    'text-align' : 'center',
-                                    'box-shadow' : 'none',
-                                    'text-shadow' : '0 1px 0 rgba(0, 0, 0, .1)',
-                                    'background-color' : '#6f5499',
-                                    'background-image' : '-webkit-gradient(linear, left top, left bottom, from(#563d7c), to(#6f5499))',
-                                    'background-image' : '-webkit-linear-gradient(top, #563d7c 0, #6f5499 100%)',
-                                    'background-image' : '-o-linear-gradient(top, #563d7c 0, #6f5499 100%)',
-                                    'background-image' : 'linear-gradient(to bottom, #563d7c 0, #6f5499 100%)',
-                                    'filter' : 'progid: DXImageTransform.Microsoft.gradient(startColorstr=\'#563d7c\', endColorstr=\'#6F5499\', GradientType=0)',
-                                    'background-repeat' : 'repeat-x'
-                                })
-                            ;
-                            $('#accordion-section-themes .accordion-section-title .change-theme').before(hypermarket_plus);
-                            // Remove accordion click event
-                            $('.get-hypermarket-plus').on('click', function(e) {
-                                e.stopPropagation();
-                            });
-                        });
-                    })(jQuery);
-                </script>
-            <?php
+                wp_localize_script('hypermarket-customizer', 'hypermarket_bs3_grid_builder', array(
+                    'bs3GridBuilderURL' => hypermarket_sanitize_url(get_admin_url()) . 'plugin-install.php?s=bs3+grid+builder&tab=search&type=term',
+                    'bs3GridBuilderLabel' => __('Get Free Grid Builder!', 'hypermarket')
+                ));
             endif;
+            wp_enqueue_script('hypermarket-customizer');
         }
     }
 endif;
