99341807bb5314e121dd1238b3598f648d8770fa
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-01-26 17:34:56 +0400

    Horizontal row added

--- template-parts/hypermarket-homepage-content.php
+++ template-parts/hypermarket-homepage-content.php
@@ -5,15 +5,15 @@
  * @package 	Hooked into "hypermarket_homepage_template"
  * @author  	Mahdi Yazdani
  * @package 	Hypermarket
- * @since 	    1.0
+ * @since 	    1.0.1
  */
 if (have_posts() &&  '' !== get_post()->post_content):
 ?>
 <!-- Homepage Content -->
 <section id="hypermarket-content" class="container padding-top-2x padding-bottom-2x">
-	<hr>
 	<?php 
 		if(hypermarket_is_woocommerce_activated()):
+			echo '<hr>';
 			echo '<p class="space-bottom">' . wp_kses_post( hypermarket_do_shortcode( 'woocommerce_messages' ) ) . '</p>';
 		endif;
 		if( apply_filters('hypermarket_homepage_page_title', true) ): 
