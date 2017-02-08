fb7978f61bedecb7f880bd68e7d9b904ee9c8bf9
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-02-08 16:20:36 +0400

    New method for displaying shop page featured image

--- template-parts/hypermarket-featured-image-background-single-page.php
+++ template-parts/hypermarket-featured-image-background-single-page.php
@@ -1,22 +1,25 @@
 <?php
 /**
- * Displaying featured image or user avatar in my account page.
+ * Displaying featured image or user gravatar in my account page.
  *
  * @package 		Hooked into "hypermarket_featured_image_single_page"
  * @author  		Mahdi Yazdani
  * @package 		Hypermarket
- * @since 		    1.0
+ * @since 		    1.0.2
  */
-if (hypermarket_is_woocommerce_activated() && is_account_page() && is_user_logged_in() && ! has_post_thumbnail()):
+if (hypermarket_is_woocommerce_activated() && is_account_page() && is_user_logged_in() && !has_post_thumbnail()):
 	echo '<!-- Featured Background -->';
 	$current_user = wp_get_current_user();
 	echo '<div class="featured-background">';
-	echo get_avatar( $current_user->user_email, 320, '', $current_user->display_name );
+	echo get_avatar($current_user->user_email, 320, '', $current_user->display_name);
 	echo '</div><!-- .featured-background -->';
+elseif (hypermarket_is_woocommerce_activated() && is_shop() && !empty(get_the_post_thumbnail_url(get_option('woocommerce_shop_page_id')))):
+	echo '<!-- Featured Image -->';
+	echo '<div class="featured-image data-background" data-background="' . get_the_post_thumbnail_url(get_option('woocommerce_shop_page_id')) . '"></div><!-- .featured-image -->';
 else:
-	if(has_post_thumbnail()):
+	if (has_post_thumbnail()):
 		global $post;
 		echo '<!-- Featured Image -->';
-		echo '<div class="featured-image data-background" data-background="' . wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' )[0] . '"></div><!-- .featured-image -->';
+		echo '<div class="featured-image data-background" data-background="' . wp_get_attachment_image_src(get_post_thumbnail_id($post->ID) , 'full') [0] . '"></div><!-- .featured-image -->';
 	endif;
 endif;
