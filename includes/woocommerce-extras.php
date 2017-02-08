a3a71824b1999aa06b43a6bba0233940aba16701
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-02-08 16:17:42 +0400

    Check to see, if WooCommerce is already enabled

--- includes/woocommerce-extras.php
+++ includes/woocommerce-extras.php
@@ -5,16 +5,18 @@
  *
  * @author      Mahdi Yazdani
  * @package     Hypermarket
- * @since       1.0
+ * @since       1.0.2
  */
 /**
  * Checks if the current page is an account page and user not logged-in.
  *
- * @since 1.0
+ * @since 1.0.2
  */
 if (!function_exists('hypermarket_is_account_page_not_logged_in')):
     function hypermarket_is_account_page_not_logged_in()
     {
-        return ( is_account_page() && ! is_user_logged_in() ) ? true : false;
+    	if (hypermarket_is_woocommerce_activated()):
+        	return ( is_account_page() && ! is_user_logged_in() ) ? true : false;
+    	endif;
     }
 endif;
