93fd0f2675051623b1f3be20c2e7b00f4cad0572
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-02-08 16:21:02 +0400

    Compatibility with Ajax Post extension

--- template-parts/hypermarket-post-entry.php
+++ template-parts/hypermarket-post-entry.php
@@ -5,14 +5,23 @@
  * @package 		Hooked into "hypermarket_loop_posts"
  * @author  		Mahdi Yazdani
  * @package 		Hypermarket
- * @since 		    1.0.1
+ * @since 		    1.0.2
  */
 ?>
 <div class="col-md-offset-1 col-sm-8">
 	<?php
-		if (has_post_thumbnail()):
-			the_post_thumbnail( 'large', array( 'class' => 'space-bottom' ) );
+		// Check for featured image toggle which may come via Ajax Post extension
+		$featured_image_status = false;
+		if(apply_filters('hypermarket_ajax_post_featured_image_toggle', false) === true):
+			$featured_image_status = get_post_meta($post->ID, '_hypermarket_ajax_post_featured_image_toggle', false);
+			if(is_array($featured_image_status) && isset($featured_image_status) && !empty($featured_image_status)):
+				$featured_image_status = true;
+			endif;
 		endif;
+		if (has_post_thumbnail() && !$featured_image_status):
+			the_post_thumbnail( 'large', array( 'class' => 'space-bottom', 'itemprop'=>'image' ) );
+		endif;
+		do_action('hypermarket_archive_featured_image');
 		if ( strpos( get_the_content(), 'more-link' ) === false ):
 			the_excerpt();
 		else:
