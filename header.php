fdb61bd34427d85a4bef0015d062e448b372a9a0
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-02-27 04:30:45 +0400

    Removed invalid attributes

--- header.php
+++ header.php
@@ -5,14 +5,14 @@
  *
  * @author      Mahdi Yazdani
  * @package     Hypermarket
- * @since       1.0.1
+ * @since       1.0.3
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <!--Mobile Specific Meta Tag-->
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="mobile-web-app-capable" content="yes" />
 <meta name="apple-mobile-web-app-capable" content="yes" />
 <meta name="apple-mobile-web-app-title" content="<?php bloginfo('name'); ?> - <?php bloginfo('description'); ?>" />
