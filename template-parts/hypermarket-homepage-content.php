af3bf6988b16c45116149fb77b862a34ee6c1777
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-02-27 04:35:22 +0400

    Removed blank paragraph tag

--- template-parts/hypermarket-homepage-content.php
+++ template-parts/hypermarket-homepage-content.php
@@ -5,7 +5,7 @@
  * @package 	Hooked into "hypermarket_homepage_template"
  * @author  	Mahdi Yazdani
  * @package 	Hypermarket
- * @since 	    1.0.1
+ * @since 	    1.0.3
  */
 if (have_posts() &&  '' !== get_post()->post_content):
 ?>
@@ -14,13 +14,13 @@ if (have_posts() &&  '' !== get_post()->post_content):
 	<?php 
 		if(hypermarket_is_woocommerce_activated()):
 			echo '<hr>';
-			echo '<p class="space-bottom">' . wp_kses_post( hypermarket_do_shortcode( 'woocommerce_messages' ) ) . '</p>';
+			echo wp_kses_post( hypermarket_do_shortcode( 'woocommerce_messages' ) );
 		endif;
 		if( apply_filters('hypermarket_homepage_page_title', true) ): 
 			echo '<h3 class="text-center padding-top-2x">' . get_the_title() . '</h3>';
 		endif;
 	?>
-	<div id="post-<?php the_ID(); ?>" <?php post_class('row padding-top'); ?>">
+	<div id="post-<?php the_ID(); ?>" <?php post_class('row padding-top'); ?>>
 		<div class="col-sm-12">
 			<?php
 				while (have_posts()):
