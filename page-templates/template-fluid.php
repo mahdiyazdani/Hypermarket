f214a267094a570fcb05d5bb2a1fa04ef329481b
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-03-11 14:26:06 +0400

    Added fluid post template support

--- page-templates/template-fluid.php
+++ page-templates/template-fluid.php
@@ -3,17 +3,22 @@
  * The template for displaying all pages with fluid width.
  * 
  * 
- * Template name: 	Fluid Template
- * @see 			https://codex.wordpress.org/Theme_Development
- * @author  		Mahdi Yazdani
- * @package 		Hypermarket
- * @since 			1.0.3
+ * Template name: 		Fluid Template
+ * Template Post Type: 	post, page
+ * @see 				https://codex.wordpress.org/Theme_Development
+ * @author  			Mahdi Yazdani
+ * @package 			Hypermarket
+ * @since 				1.0.4
  */
 
 get_header();
 
 if ( have_posts() ) :
-	get_template_part( 'loop-templates/content', 'page' );
+	if(is_page()):
+		get_template_part( 'loop-templates/content', 'page' );
+	else:
+		get_template_part( 'loop-templates/content', 'post' );
+	endif;
 else:
 	get_template_part( 'loop-templates/content', 'none' );
 endif;
