eb87af59fa2234614b86e490d948637d80a1778c
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-03-15 03:42:33 +0400

    Updated related template file to meet WC 2.6.14

--- woocommerce/single-product/related.php
+++ woocommerce/single-product/related.php
@@ -13,15 +13,39 @@
  * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     2.7.0
+ * @version     1.6.4
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( $related_products ) : ?>
+global $product, $woocommerce_loop;
 
+if ( empty( $product ) || ! $product->exists() ) {
+	return;
+}
+
+if ( ! $related = $product->get_related( $posts_per_page ) ) {
+	return;
+}
+
+$args = apply_filters( 'woocommerce_related_products_args', array(
+	'post_type'            => 'product',
+	'ignore_sticky_posts'  => 1,
+	'no_found_rows'        => 1,
+	'posts_per_page'       => $posts_per_page,
+	'orderby'              => $orderby,
+	'post__in'             => $related,
+	'post__not_in'         => array( $product->id )
+) );
+
+$products                    = new WP_Query( $args );
+$woocommerce_loop['name']    = 'related';
+$woocommerce_loop['columns'] = apply_filters( 'woocommerce_related_products_columns', $columns );
+
+if ( $products->have_posts() ) : ?>
+	
 	<!-- Related Products -->
 	<section class="container padding-top padding-bottom">
 		<hr>
@@ -31,14 +55,12 @@ if ( $related_products ) : ?>
 				woocommerce_product_loop_start();
 				// 4 Columns
 				$GLOBALS['product_grid_classes'] = 'col-lg-3 col-sm-6';
-				foreach ( $related_products as $related_product ) :
-					$post_object = get_post( $related_product->get_id() );
-					setup_postdata( $GLOBALS['post'] =& $post_object );
+				while ( $products->have_posts() ) : $products->the_post();
 					wc_get_template_part( 'content', 'product' );
-				endforeach; // end of the loop.
+				endwhile; // end of the loop.
 				woocommerce_product_loop_end(); 
 			?>
-			</div><!-- .row -->
+		</div><!-- .row -->
 	</section><!-- .container -->
 
 <?php endif;
