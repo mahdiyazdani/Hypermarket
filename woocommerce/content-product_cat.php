3bf3f5fd2ce99db0f14e33124ddf89ac899363eb
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-03-14 11:52:49 +0400

    Added new tag element wrapper to display WooCommerce subcategory

--- woocommerce/content-product_cat.php
+++ woocommerce/content-product_cat.php
@@ -20,42 +20,38 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 ?>
-<div class="hypermarket-category-wrapper row padding-top padding-bottom-3x">
-	<div <?php wc_product_cat_class( $GLOBALS['product_grid_classes'], $category ); ?>>
-		<?php
-		/**
-		 * woocommerce_before_subcategory hook.
-		 *
-		 * @hooked woocommerce_template_loop_category_link_open - 10
-		 */
-		do_action( 'woocommerce_before_subcategory', $category );
+<div <?php wc_product_cat_class( $GLOBALS['product_grid_classes'], $category ); ?>>
+	<?php
+	/**
+	 * woocommerce_before_subcategory hook.
+	 *
+	 * @hooked woocommerce_template_loop_category_link_open - 10
+	 */
+	do_action( 'woocommerce_before_subcategory', $category );
 
-		/**
-		 * woocommerce_before_subcategory_title hook.
-		 *
-		 * @hooked woocommerce_subcategory_thumbnail - 10
-		 */
-		do_action( 'woocommerce_before_subcategory_title', $category );
+	/**
+	 * woocommerce_before_subcategory_title hook.
+	 *
+	 * @hooked woocommerce_subcategory_thumbnail - 10
+	 */
+	do_action( 'woocommerce_before_subcategory_title', $category );
 
-		/**
-		 * woocommerce_shop_loop_subcategory_title hook.
-		 *
-		 * @hooked woocommerce_template_loop_category_title - 10
-		 */
-		do_action( 'woocommerce_shop_loop_subcategory_title', $category );
+	/**
+	 * woocommerce_shop_loop_subcategory_title hook.
+	 *
+	 * @hooked woocommerce_template_loop_category_title - 10
+	 */
+	do_action( 'woocommerce_shop_loop_subcategory_title', $category );
 
-		/**
-		 * woocommerce_after_subcategory_title hook.
-		 */
-		do_action( 'woocommerce_after_subcategory_title', $category );
+	/**
+	 * woocommerce_after_subcategory_title hook.
+	 */
+	do_action( 'woocommerce_after_subcategory_title', $category );
 
-		/**
-		 * woocommerce_after_subcategory hook.
-		 *
-		 * @hooked woocommerce_template_loop_category_link_close - 10
-		 */
-		do_action( 'woocommerce_after_subcategory', $category ); ?>
-	</div><!-- .product-category -->
-</div><!-- .hypermarket-category-wrapper -->
-<?php 
-woocommerce_reset_loop();
+	/**
+	 * woocommerce_after_subcategory hook.
+	 *
+	 * @hooked woocommerce_template_loop_category_link_close - 10
+	 */
+	do_action( 'woocommerce_after_subcategory', $category ); ?>
+</div><!-- .product-category -->
