baa1259ac417b038796c63be95e1781c178adade
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-03-16 05:54:13 +0400

    Added product not found template markup

--- template-parts/woocommerce/hypermarket-no-products-found.php
+++ template-parts/woocommerce/hypermarket-no-products-found.php
@@ -1,24 +1,13 @@
 <?php
 /**
- * Displayed when no products are found matching the current query
+ * Displayed when no products are found matching the current query.
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/loop/no-products-found.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     2.0.0
+ * @package 		Hooked into "woocommerce_no_products_found"
+ * @author  		Mahdi Yazdani
+ * @package 		Hypermarket
+ * @since 		    1.0.4
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
 remove_action('woocommerce_sidebar', 'woocommerce_get_sidebar');
 ?>
 <div class="row padding-top">
