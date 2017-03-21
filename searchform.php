a509b62c8a419e93ffc847346bb35b15b2a2b651
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-03-22 02:49:14 +0400

    Added escaping functions

--- searchform.php
+++ searchform.php
@@ -4,11 +4,11 @@
  *
  * @author      Mahdi Yazdani
  * @package     Hypermarket
- * @since       1.0
+ * @since       1.0.4.1
  */
  ?>
-<form method="get" id="searchform" action="<?php echo hypermarket_sanitize_url( home_url( '/' ) ); ?>" class="search-box">
-	<label class="screen-reader-text" for="s"><?php _e( 'Search for:', 'hypermarket' ); ?></label>
+<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" class="search-box">
+	<label class="screen-reader-text" for="s"><?php esc_html_e( 'Search for:', 'hypermarket' ); ?></label>
 	<input name="s" id="s" type="text" class="form-control" placeholder="<?php esc_attr_e( 'Search', 'hypermarket' ); ?>" required="required" value="<?php echo get_search_query(); ?>" />
 	<button type="submit">
 		<i class="material-icons search"></i>
