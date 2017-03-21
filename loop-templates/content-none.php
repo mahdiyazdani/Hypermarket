db195dda5523282c8cca6a099d6556f551cc4315
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-03-22 02:48:59 +0400

    Added escaping functions

--- loop-templates/content-none.php
+++ loop-templates/content-none.php
@@ -5,28 +5,28 @@
  * @see 			http://codex.wordpress.org/Template_Hierarchy
  * @author  		Mahdi Yazdani
  * @package 		Hypermarket
- * @since 		    1.0
+ * @since 		    1.0.4.1
  */
 ?>
 <!-- Content -->
 <section class="container padding-top-3x">
-	<h1 class="mobile-center"><?php _e( 'Nothing Found', 'hypermarket' ); ?></h1>
+	<h1 class="mobile-center"><?php esc_html_e( 'Nothing Found', 'hypermarket' ); ?></h1>
 		<div class="row padding-top">
 			<div class="col-md-12">
 				<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-					<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.',
+					<p><?php printf( esc_html__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.',
 					'hypermarket' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 
 				<?php elseif ( is_search() ) : ?>
 
-					<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.',
+					<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.',
 					'hypermarket' ); ?></p>
 					<?php get_search_form(); ?>
 
 				<?php else : ?>
 
-					<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.',
+					<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.',
 					'hypermarket' ); ?></p>
 
 					<?php get_search_form(); ?>
