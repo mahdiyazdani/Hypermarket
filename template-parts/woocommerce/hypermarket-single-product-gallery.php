0d75b2075e7855c15456bcd76f8ce57a532131e0
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-03-22 02:50:51 +0400

    Added escaping functions

--- template-parts/woocommerce/hypermarket-single-product-gallery.php
+++ template-parts/woocommerce/hypermarket-single-product-gallery.php
@@ -5,7 +5,7 @@
  * @package 		Hooked into "woocommerce_before_single_product_summary"
  * @author  		Mahdi Yazdani
  * @package 		Hypermarket
- * @since 		    1.0.4
+ * @since 		    1.0.4.1
  */
 global $post, $product, $woocommerce;
 
@@ -28,7 +28,7 @@ $attachment_ids = $product->get_gallery_attachment_ids();
 						'data-large-image-width'  => $full_size_image[1],
 						'data-large-image-height' => $full_size_image[2],
 					);
-					echo '<li id="preview01" data-thumb="' . get_the_post_thumbnail_url( $post->ID, 'shop_thumbnail' ) . '" class="current woocommerce-product-gallery__image">';
+					echo '<li id="preview01" data-thumb="' . esc_url(get_the_post_thumbnail_url( $post->ID, 'shop_thumbnail' )) . '" class="current woocommerce-product-gallery__image">';
 						echo get_the_post_thumbnail( $post->ID, 'shop_single', $attributes );
 					echo '</li><!-- .woocommerce-product-gallery__image -->';
 					if(is_array($attachment_ids) && ! empty($attachment_ids)):
@@ -44,7 +44,7 @@ $attachment_ids = $product->get_gallery_attachment_ids();
 								'data-large-image-width'  => $full_size_image[1],
 								'data-large-image-height' => $full_size_image[2],
 							);
-							echo '<li id="preview0' . $counter . '" data-thumb="' . get_the_post_thumbnail_url( $post->ID, 'shop_thumbnail' ) . '" class="woocommerce-product-gallery__image">';
+							echo '<li id="preview0' . esc_attr($counter) . '" data-thumb="' . esc_url(get_the_post_thumbnail_url( $post->ID, 'shop_thumbnail' )) . '" class="woocommerce-product-gallery__image">';
 								echo wp_get_attachment_image( $attachment_id, 'shop_single', false, $attributes );
 							echo '</li><!-- .woocommerce-product-gallery__image -->';
 							$counter++;
@@ -65,7 +65,7 @@ $attachment_ids = $product->get_gallery_attachment_ids();
 					'data-large-image-width'  => $full_size_image[1],
 					'data-large-image-height' => $full_size_image[2],
 				);
-				echo '<li data-thumb="' . get_the_post_thumbnail_url( $post->ID, 'shop_thumbnail' ) . '" class="active woocommerce-product-gallery__image">';
+				echo '<li data-thumb="' . esc_url(get_the_post_thumbnail_url( $post->ID, 'shop_thumbnail' )) . '" class="active woocommerce-product-gallery__image">';
 					echo '<a href="#preview01">';
 						echo get_the_post_thumbnail( $post->ID, 'shop_single', $attributes );
 					echo '</a>';
@@ -83,8 +83,8 @@ $attachment_ids = $product->get_gallery_attachment_ids();
 							'data-large-image-width'  => $full_size_image[1],
 							'data-large-image-height' => $full_size_image[2],
 						);
-						echo '<li data-thumb="' . get_the_post_thumbnail_url( $post->ID, 'shop_thumbnail' ) . '" class="woocommerce-product-gallery__image">';
-							echo '<a href="#preview0' . $counter . '">';
+						echo '<li data-thumb="' . esc_url(get_the_post_thumbnail_url( $post->ID, 'shop_thumbnail' )) . '" class="woocommerce-product-gallery__image">';
+							echo '<a href="#preview0' . esc_attr($counter) . '">';
 								echo wp_get_attachment_image( $attachment_id, 'shop_thumbnail', false, $attributes );
 							echo '</a>';
 						echo '</li><!-- .woocommerce-product-gallery__image -->';
