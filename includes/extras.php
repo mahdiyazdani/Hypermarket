cc9dc308c9156e025930a5415da61580b2dec9a8
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-03-22 02:47:55 +0400

    Removed URL escape method

--- includes/extras.php
+++ includes/extras.php
@@ -5,7 +5,7 @@
  *
  * @author      Mahdi Yazdani
  * @package     Hypermarket
- * @since       1.0.4
+ * @since       1.0.4.1
  */
 /**
  * Query WooCommerce activation.
@@ -94,17 +94,6 @@ if (!function_exists('hypermarket_sanitize_textarea')):
     }
 endif;
 /**
- * Sanitize URL
- *
- * @since 1.0
- */
-if (!function_exists('hypermarket_sanitize_url')):
-    function hypermarket_sanitize_url($url)
-    {
-        return esc_url($url);
-    }
-endif;
-/**
  * Sanitize Checkbox
  * Sanitization callback for 'checkbox' type controls. This callback sanitizes `$checked`
  * as a boolean value, either TRUE or FALSE.
@@ -161,7 +150,7 @@ endif;
 /**
  * Comments list template.
  *
- * @since 1.0.4
+ * @since 1.0.4.1
  */
 if (!function_exists('hypermarket_comments_list')):
     function hypermarket_comments_list($comment, $args, $depth)
@@ -178,28 +167,28 @@ if (!function_exists('hypermarket_comments_list')):
                 <div class="column">
                     <p class="meta">
                         <strong itemprop="author"><?php
-        echo (!empty(get_comment_author_url($comment))) ? '<a href="' . get_comment_author_url($comment) . '" class="hypermarket-link-style" target="_blank" rel="external nofollow">' . get_comment_author($comment) . '</a>' : get_comment_author($comment); ?></strong> – <?php
-        echo '<a href="' . get_comment_link($comment) . '" class="comment-permalink scroll-to" target="_self" data-comment-id="comment-' . get_comment_ID() . '"><time datetime="' . get_comment_date('c') . '">' . get_comment_date() . '</time></a>'; ?>:
+                            echo (!empty(get_comment_author_url($comment))) ? '<a href="' . esc_url(get_comment_author_url($comment)) . '" class="hypermarket-link-style" target="_blank" rel="external nofollow">' . esc_html(get_comment_author($comment)) . '</a>' : esc_html(get_comment_author($comment)); ?></strong> – <?php
+                            echo '<a href="' . esc_url(get_comment_link($comment)) . '" class="comment-permalink scroll-to" target="_self" data-comment-id="comment-' . esc_attr(get_comment_ID()) . '"><time datetime="' . esc_attr(get_comment_date('c')) . '">' . esc_html(get_comment_date()) . '</time></a>';         ?>:
                     </p><!-- .meta -->
                 </div><!-- .column -->
                 <div class="column">
                   <span class="comment-reply">
                     <?php
-        comment_reply_link(array_merge($args, array(
-            'reply_text' => '<i class="material-icons reply"></i>',
-            'depth' => $depth,
-            'max_depth' => $args['max_depth']
-        )));
-?>
+                        comment_reply_link(array_merge($args, array(
+                            'reply_text' => '<i class="material-icons reply"></i>',
+                            'depth' => $depth,
+                            'max_depth' => $args['max_depth']
+                        )));
+                    ?>
                   </span><!-- .comment-reply -->
                 </div><!-- .column -->
               </div><!-- .comment-meta -->
               <?php
-        comment_text($comment);
-        if ('0' == $comment->comment_approved):
-            echo '<em class="hypermarket-comment-awaiting-moderation">' . __('Your comment is awaiting moderation.', 'hypermarket') . '</em>' . PHP_EOL;
-        endif;
-?>
+                    comment_text($comment);
+                    if ('0' == $comment->comment_approved):
+                        echo '<em class="hypermarket-comment-awaiting-moderation">' . esc_html__('Your comment is awaiting moderation.', 'hypermarket') . '</em>' . PHP_EOL;
+                    endif;
+                ?>
             </div><!-- .comment-body -->
             <?php
     }
@@ -209,7 +198,7 @@ endif;
  * Hypermarket theme comes as standard with a free download link mark. If you wish to remove this and update it with your text, you need to purchase Hypermarket Copyright Credit plugin.
  * 
  * @link https://wp.me/p8930x-c2
- * @since 1.0.4
+ * @since 1.0.4.1
  */
 if (!function_exists('hypermarket_credits')):
     function hypermarket_credits()
@@ -217,7 +206,7 @@ if (!function_exists('hypermarket_credits')):
         echo '<p class="copyright space-top"><span>' . apply_filters('hypermarket_copyright_text', $content = '&copy; ' . get_bloginfo('name') . ' ' . date('Y')) . '</span>';
         if( apply_filters( 'hypermarket_credit_link', true ) ):
             // You `HAVE` to keep this credit link. We really do appreciate it ;)
-            printf(esc_attr__(' | Get %1$s for free.', 'hypermarket') , '<a href="' . esc_url( HypermarketThemeURI ) . '" rel="author" target="_blank">' . HypermarketThemeName . '</a>');
+            printf(esc_attr__(' | Get %1$s for free.', 'hypermarket') , '<a href="' . esc_url( HypermarketThemeURI ) . '" rel="author" target="_blank">' . esc_attr(HypermarketThemeName) . '</a>');
         endif;
         echo '</p>' . PHP_EOL;
     }
