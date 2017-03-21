00cf806153ab9326810e9c30c401f09788c4c33b
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-03-22 02:49:51 +0400

    Added escaping functions

--- template-parts/hypermarket-loop-posts-header-image.php
+++ template-parts/hypermarket-loop-posts-header-image.php
@@ -5,9 +5,9 @@
  * @package 		Hooked into "hypermarket_before_loop_posts"
  * @author  		Mahdi Yazdani
  * @package 		Hypermarket
- * @since 		    1.0
+ * @since 		    1.0.4.1
  */
-$header_image = hypermarket_sanitize_url( get_header_image() );
+$header_image = esc_url( get_header_image() );
 if( ! empty($header_image) ):
 	echo '<!-- Featured Image -->';
 	echo '<div class="featured-image data-background" data-background="' . $header_image . '"></div><!-- .featured-image -->';
