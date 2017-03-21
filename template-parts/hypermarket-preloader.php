15529d466886bfed6e0fc5a229bd8d2744329019
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-03-22 02:50:28 +0400

    Added escaping functions

--- template-parts/hypermarket-preloader.php
+++ template-parts/hypermarket-preloader.php
@@ -5,12 +5,12 @@
  * @package 	Hooked into "hypermarket_before_header_area"
  * @author  	Mahdi Yazdani
  * @package 	Hypermarket
- * @since 	    1.0.1
+ * @since 	    1.0.4.1
  */
 ?>
 <!-- Page Pre-Loader -->
 <div class="page-preloader">
 	<div class="preloader">
-		<img src="<?php echo hypermarket_sanitize_url(get_stylesheet_directory_uri()); ?>/assets/img/preloader.gif" alt="<?php __('Preloader', 'hypermarket'); ?>" width="64" height="64" />
+		<img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/assets/img/preloader.gif" alt="<?php __('Preloader', 'hypermarket'); ?>" width="64" height="64" />
 	</div><!-- .preloader -->
 </div><!-- .page-preloader -->
