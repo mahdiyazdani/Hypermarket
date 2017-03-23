759ccc6aa987f46e80a9583792a2561058829179
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-03-23 15:43:30 +0400

    Added new hooks to override WooCommerce methods

--- includes/woocommerce-template-hooks.php
+++ includes/woocommerce-template-hooks.php
@@ -4,7 +4,7 @@
  *
  * @author  	Mahdi Yazdani
  * @package 	Hypermarket
- * @since 	    1.0.4
+ * @since 	    1.0.4.2
  */
 /**
  * Items present in cart
@@ -23,12 +23,12 @@ endif;
  *
  * @see  hypermarket_shop_featured_image()						 -> woocommerce-template-functions.php
  * @see  hypermarket_shop_thumbnail_wrapper_start()				 -> woocommerce-template-functions.php
- * @see  woocommerce_show_product_loop_sale_flash()				 -> woocommerce-template-functions.php
+ * @see  hypermarket_show_product_loop_sale_flash()				 -> woocommerce-template-functions.php
  * @see  hypermarket_shop_item_tools_wrapper_start()			 -> woocommerce-template-functions.php
  * @see  hypermarket_shop_thumbnail_item_tools_wrapper_end()	 -> woocommerce-template-functions.php
  * @see  hypermarket_shop_item_details_wrapper_start()			 -> woocommerce-template-functions.php
- * @see  woocommerce_template_loop_product_title()				 -> woocommerce-template-functions.php
- * @see  woocommerce_template_loop_price()						 -> woocommerce-template-functions.php
+ * @see  hypermarket_template_loop_product_title()				 -> woocommerce-template-functions.php
+ * @see  hypermarket_template_loop_price()						 -> woocommerce-template-functions.php
  * @see  hypermarket_shop_item_details_wrapper_end()			 -> woocommerce-template-functions.php
  * @see  hypermarket_woocommerce_before_shop_loop()				 -> woocommerce-template-functions.php
  * @see  hypermarket_shop_bar_wrapper_start()					 -> woocommerce-template-functions.php
@@ -39,7 +39,7 @@ endif;
  * @see  hypermarket_paging_navigation()						 -> woocommerce-template-functions.php
  * @see  hypermarket_woocommerce_after_shop_loop()				 -> woocommerce-template-functions.php
  * @see  hypermarket_shop_catalog_wrapper_end()					 -> woocommerce-template-functions.php
- * @since 1.0.2
+ * @since 1.0.4.2
  */
 remove_action('woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10);
 remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5);
@@ -52,15 +52,15 @@ remove_action('woocommerce_after_shop_loop', 'woocommerce_pagination', 10);
 add_action('wp', 'hypermarket_remove_sidebar_shop');
 add_action('woocommerce_before_main_content', 'hypermarket_shop_featured_image', 5);
 add_action('woocommerce_before_shop_loop_item', 'hypermarket_shop_thumbnail_wrapper_start', 10);
-add_action('woocommerce_before_shop_loop_item', 'woocommerce_show_product_loop_sale_flash', 15);
+add_action('woocommerce_before_shop_loop_item', 'hypermarket_show_product_loop_sale_flash', 10);
 add_action('woocommerce_before_shop_loop_item', 'woocommerce_template_loop_rating', 20);
 add_action('woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 25);
 add_action('woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_close', 30);
 add_action('woocommerce_before_shop_loop_item_title', 'hypermarket_shop_item_tools_wrapper_start', 15);
 add_action('woocommerce_after_shop_loop_item', 'hypermarket_shop_thumbnail_item_tools_wrapper_end', 15);
 add_action('woocommerce_after_shop_loop_item', 'hypermarket_shop_item_details_wrapper_start', 20);
-add_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_title', 25);
-add_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_price', 30);
+add_action('woocommerce_after_shop_loop_item', 'hypermarket_template_loop_product_title', 25);
+add_action('woocommerce_after_shop_loop_item', 'hypermarket_template_loop_price', 30);
 add_action('woocommerce_after_shop_loop_item', 'hypermarket_shop_item_details_wrapper_end', 35);
 add_action('woocommerce_before_shop_loop', 'hypermarket_woocommerce_before_shop_loop', 10);
 add_action('woocommerce_before_shop_loop', 'hypermarket_shop_bar_wrapper_start', 12);
@@ -88,24 +88,26 @@ add_filter('woocommerce_subcategory_count_html', '__return_false', 10);
 /**
  * Single product
  *
- * @see  woocommerce_show_product_images()						 -> woocommerce-template-functions.php
  * @see  hypermarket_product_paging_navigation()				 -> woocommerce-template-functions.php
  * @see  hypermarket_product_notice_wrapper_start()				 -> woocommerce-template-functions.php
  * @see  hypermarket_product_notice_wrapper_end()				 -> woocommerce-template-functions.php
+ * @see  hypermarket_show_product_images()						 -> woocommerce-template-functions.php
  * @see  hypermarket_single_product_summary_wrapper_start()		 -> woocommerce-template-functions.php
  * @see  hypermarket_single_add_to_cart_wrapper_start()			 -> woocommerce-template-functions.php
  * @see  hypermarket_single_add_to_cart_wrapper_end()			 -> woocommerce-template-functions.php
  * @see  hypermarket_single_product_summary_wrapper_end()		 -> woocommerce-template-functions.php
  * @see  hypermarket_output_product_data_tabs_wrapper_start()	 -> woocommerce-template-functions.php
  * @see  hypermarket_output_product_data_tabs_wrapper_end()		 -> woocommerce-template-functions.php
- * @since 1.0.2
+ * @since 1.0.4.2
  */
 remove_action('woocommerce_before_single_product_summary', 'woocommerce_show_product_sale_flash', 10);
+remove_action('woocommerce_before_single_product_summary', 'woocommerce_show_product_images', 20);
 remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10);
 remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
 add_action('woocommerce_before_single_product', 'hypermarket_product_paging_navigation', 7);
 add_action('woocommerce_before_single_product', 'hypermarket_product_notice_wrapper_start', 9);
 add_action('woocommerce_before_single_product', 'hypermarket_product_notice_wrapper_end', 11);
+add_action('woocommerce_before_single_product_summary', 'hypermarket_show_product_images', 20);
 add_action('woocommerce_before_single_product_summary', 'hypermarket_single_product_summary_wrapper_start', 30);
 add_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 22);
 add_action('woocommerce_single_product_summary', 'woocommerce_template_single_rating', 25);
