f28155bd50be1f5096bd1278d6d562a982ccf5b4
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-04-06 20:50:52 +0400

    Removed PHP_EOL

--- template-parts/hypermarket-header-toolbar.php
+++ template-parts/hypermarket-header-toolbar.php
@@ -15,11 +15,11 @@
             if(!hypermarket_is_woocommerce_activated()):
                 echo '<a href="#" class="mobile-menu-toggle menu-text-right"><i class="material-icons menu"></i></a>';
             else:
-                echo '<a href="#" class="mobile-menu-toggle"><i class="material-icons menu"></i></a>' . PHP_EOL;
+                echo '<a href="#" class="mobile-menu-toggle"><i class="material-icons menu"></i></a>';
             endif;
         	// Append WooCommerce my-account page link
         	if (apply_filters('hypermarket_header_toolbar_myaccount', true) && hypermarket_is_woocommerce_activated() && get_option('users_can_register')):
-        		echo '<a href="' . esc_url(get_permalink( get_option('woocommerce_myaccount_page_id') )) . '" target="_self"><i class="material-icons person"></i></a>' . PHP_EOL;
+        		echo '<a href="' . esc_url(get_permalink( get_option('woocommerce_myaccount_page_id') )) . '" target="_self"><i class="material-icons person"></i></a>';
         	endif;
             // Append WooCommerce Mini Cart
         	if (apply_filters('hypermarket_header_toolbar_mini_cart', true) && hypermarket_is_woocommerce_activated()):
