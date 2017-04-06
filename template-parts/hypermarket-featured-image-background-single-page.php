7d27d568aa2f5ea42f48e95e25740cc816b03b00
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-04-06 20:50:40 +0400

    Added new conditional statement to display shop page featured image

--- template-parts/hypermarket-featured-image-background-single-page.php
+++ template-parts/hypermarket-featured-image-background-single-page.php
@@ -5,7 +5,7 @@
  * @package 		Hooked into "hypermarket_featured_image_single_page"
  * @author  		Mahdi Yazdani
  * @package 		Hypermarket
- * @since 		    1.0.4.1
+ * @since 		    1.0.5
  */
 if (hypermarket_is_woocommerce_activated() && is_account_page() && is_user_logged_in() && !has_post_thumbnail()):
 	echo '<!-- Featured Background -->';
@@ -13,9 +13,12 @@ if (hypermarket_is_woocommerce_activated() && is_account_page() && is_user_logge
 	echo '<div class="featured-background">';
 	echo get_avatar($current_user->user_email, 320, '', $current_user->display_name);
 	echo '</div><!-- .featured-background -->';
-elseif (hypermarket_is_woocommerce_activated() && is_shop() && !empty(get_the_post_thumbnail_url(get_option('woocommerce_shop_page_id')))):
-	echo '<!-- Featured Image -->';
-	echo '<div class="featured-image data-background" data-background="' . esc_url(get_the_post_thumbnail_url(get_option('woocommerce_shop_page_id'))) . '"></div><!-- .featured-image -->';
+elseif (hypermarket_is_woocommerce_activated() && is_shop()):
+	$get_shop_page_featured_image_url = get_the_post_thumbnail_url(get_option('woocommerce_shop_page_id'));
+	if (!empty($get_shop_page_featured_image_url)):
+		echo '<!-- Featured Image -->';
+		echo '<div class="featured-image data-background" data-background="' . esc_url(get_the_post_thumbnail_url(get_option('woocommerce_shop_page_id'))) . '"></div><!-- .featured-image -->';
+	endif;
 else:
 	if (has_post_thumbnail()):
 		global $post;
