364b1cd3ba36a8b0b13629094cd24a3ffb5899e1
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-08-01 18:29:33 +0400

    Added new filter to disable homepage content

--- template-parts/hypermarket-homepage-content.php
+++ template-parts/hypermarket-homepage-content.php
@@ -5,22 +5,22 @@
  * @package 	Hooked into "hypermarket_homepage_template"
  * @author  	Mahdi Yazdani
  * @package 	Hypermarket
- * @since 	    1.0.3
+ * @since 	    1.0.9.0
  */
 if (have_posts() &&  '' !== get_post()->post_content):
 ?>
 <!-- Homepage Content -->
 <section id="hypermarket-content" class="container padding-top padding-bottom space-top space-bottom">
 	<?php 
-		if(hypermarket_is_woocommerce_activated()):
+		if (hypermarket_is_woocommerce_activated()):
 			echo '<hr>';
-			echo wp_kses_post( hypermarket_do_shortcode( 'woocommerce_messages' ) );
+			echo wp_kses_post(hypermarket_do_shortcode('woocommerce_messages'));
 		endif;
-		if( apply_filters('hypermarket_homepage_page_title', true) ): 
-			echo '<h3 class="text-center padding-top-2x">' . get_the_title() . '</h3>';
+		if (apply_filters('hypermarket_homepage_page_title', true)):
+			echo '<h3 class="text-center padding-top-2x space-bottom-none">' . get_the_title() . '</h3>';
 		endif;
 	?>
-	<div id="post-<?php the_ID(); ?>" <?php post_class('row padding-top'); ?>>
+	<div id="post-<?php the_ID(); ?>" <?php post_class('row padding-top-2x'); ?>>
 		<div class="col-sm-12">
 			<?php
 				while (have_posts()):
