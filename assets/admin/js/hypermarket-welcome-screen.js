90caf081f098d3b09a04a01c1480fa43756a84ff
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-09-27 20:47:25 +0400

    Removed extra spaces

--- assets/admin/js/hypermarket-welcome-screen.js
+++ assets/admin/js/hypermarket-welcome-screen.js
@@ -3,20 +3,17 @@
  *
  * @author      Mahdi Yazdani
  * @package     Hypermarket
- * @since       1.0.3.1
+ * @since       1.0.7.0
  */
 
 jQuery(document).ready(function($) {
     'use strict';
 
     var current_tab = 'welcome';
-
     // Hide all content(s) and sidebar(s).
     hypermarketHideAllWelcomeScreenTabs();
-
     // Show current selected tab content and sidebar.
     hypermarketShowSelectedWelcomeScreenContent(current_tab);
-    
     // Handle click event on welcome screen navigation item(s).
     $('#hypermarket-welcome-screen-nav > a').on('click', function(event){
     	event.preventDefault();
@@ -26,7 +23,6 @@ jQuery(document).ready(function($) {
     	hypermarketHideAllWelcomeScreenTabs();
     	hypermarketShowSelectedWelcomeScreenContent(current_tab);
     });
-
     function hypermarketHideAllWelcomeScreenTabs() {
     	$('#hypermarket-welcome-screen-nav > a').each(function(){
     		var tabID = $(this).attr('href').replace('#', '');
@@ -35,6 +31,12 @@ jQuery(document).ready(function($) {
     }
     function hypermarketShowSelectedWelcomeScreenContent(selector) {
     	$('#' + selector + '-content, #' + selector + '-sidebar').show();
+    }
+    // Redirect to "Go Plus" tab on page load
+    if(window.location.href.indexOf('#goplus') > -1){
+        setTimeout(function(){
+            $('#hypermarket-welcome-screen-nav > a.goplus').trigger('click');
+        }, 500);
     }
 
 }); /*Document Ready End*/
